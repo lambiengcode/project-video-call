2c9ee966209ba62632109edf438e753a2cffff1d
lambiengcode <lambiengcode@gmail.com>
2021-10-27 09:47:18 +0700

    fix

--- server/index.js
+++ server/index.js
@@ -7,6 +7,7 @@ const server = require("http").Server(app);
 const io = require("socket.io")(server);
 const port = process.env.PORT || 8000;
 
+app.use(express.static("public"));
 app.use(cors());
 
 app.get("/", (req, res) => {
@@ -28,7 +29,7 @@ io.on("connection", function (socket) {
   });
 
   socket.on("call", function (data) {
-      io.emit('call', data);
+    io.emit("call", data);
   });
 });
 
